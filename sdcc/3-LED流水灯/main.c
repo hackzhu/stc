5b8f716fb23129d33c24a9d427004c3b1ba930dc
hackzhu <3110497917@qq.com>
2020-07-05 08:49:51 +0800

    failed 移位

---
+++ sdcc/3-LED流水灯/main.c
@@ -0,0 +1,16 @@
+#include<8052.h>
+
+void main()
+{
+	unsigned int i=0,j=0;
+	P2=0xfe;
+	for(i=0;i<30000;i++);
+	while(1)
+	{
+		for(j=0;j<8;j++)
+		{
+			P2>>=3;
+			for(i=0;i<30000;i++);
+		}
+	}
+}
