6925f9fb15bf838234578d48da1cf02d590c5a5d
hackzhu <3110497917@qq.com>
2020-07-17 12:20:05 +0000

    2-LED闪烁/

--- sdcc/2-LED闪烁/main.c
+++ sdcc/2-LED闪烁/main.c
@@ -2,11 +2,11 @@
 
 void main()
 {
-	unsigned int i=0;
+	unsigned int i=0;		//unsigned int:0~65535
 	while(1)
 	{
 		P2_7=1;
-		for(i=0;i<30000;i++);
+		for(i=0;i<30000;i++);	//延时函数
 		P2_7=0;
 		for(i=0;i<30000;i++);
 	}
