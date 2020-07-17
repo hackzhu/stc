d4ea73a5d74d2a9801e26245bb858e4e1ec4ac62
hackzhu <3110497917@qq.com>
2020-07-17 12:21:27 +0000

    3-LED流水灯/

--- sdcc/3-LED流水灯/main.c
+++ sdcc/3-LED流水灯/main.c
@@ -3,13 +3,12 @@
 void main()
 {
 	unsigned int i=0,j=0;
-//	P2=0xfe;
 	for(i=0;i<30000;i++);
 	while(1)
 	{
 		for(j=0;j<8;j++)
 		{
-			P2=0xff<<j;
+			P2=0xff<<j;	11111111 ...--> 11110000 ...--> 00000000
 			for(i=0;i<30000;i++);
 		}
 	}
