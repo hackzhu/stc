9481ce23d1494b6f4a872fc9df65c48cbe40526e
hackzhu <3110497917@qq.com>
2020-07-22 16:03:44 +0000

    添加外部中断1注释

--- sdcc/12-外部中断/main.c
+++ sdcc/12-外部中断/main.c
@@ -1,6 +1,7 @@
 #include"8052.h"
-
+//注释的是外部中断1
 void int0() __interrupt 0
+//void int0() __interrupt 2
 {
 	unsigned int i=1000;
 	while(i--);
@@ -11,7 +12,9 @@ void main()
 {
 	EA=1;		//打开中断总开关
 	EX0=1;		//打开外部中断0开关
-	IT0=1;		
+	IT0=1;		//跳变沿出发方式（下降沿）		
+//	EX1=1;		//打开外部中断1开关
+//	IT1=1;
 	while(1);
 }
 
