f9f47f27fa91af16801f8fcae07c5ce8e1f7a45c
hackzhu <3110497917@qq.com>
2020-07-23 17:45:57 +0000

    参照sdcc生成的asm文件

--- asm/2-LED闪烁/main.asm
+++ asm/2-LED闪烁/main.asm
@@ -4,10 +4,27 @@
 .org 0x0000
 P2_0=0x00A0
 start:
-leding:
-clr P2_0
-mov cx, 1000
-timedelay:
-loop:timedelay
-setb P2_0
-loop:ledring
+nooverloop:
+clr		P2_0
+	mov	r6,#0x30
+	mov	r7,#0x75
+00108$:
+	dec	r6
+	cjne	r6,#0xff,00136$
+	dec	r7
+00136$:
+	mov	a,r6
+	orl	a,r7
+	jnz	00108$
+setb	P2_0
+	mov	r6,#0x30
+	mov	r7,#0x75
+00118$:
+	dec	r6
+	cjne	r6,#0xff,00116$
+	dec	r7
+00116$:
+	mov	a,r6
+	orl	a,r7
+	jnz	00118$
+sjmp nooverloop
